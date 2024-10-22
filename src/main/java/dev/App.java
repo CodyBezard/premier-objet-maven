059cf869c2677e87a64274c692557b2fe0191bb5
CodyBezard <cody.bezard@gmail.com>
2024-10-22 18:41:39 +0200

    TP 1 et 2

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,14 +1,20 @@
 package dev;
 
+import com.github.lalyos.jfiglet.FigletFont;
+
+import java.io.IOException;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args ){
+    public static void main( String[] args ) throws IOException {
 
-        System.out.println( "Hello World!" );
+//        System.out.println( "Hello World!" );
+        String hello = FigletFont.convertOneLine("Hello World");
+        System.out.println(hello);
 
     }
 }
